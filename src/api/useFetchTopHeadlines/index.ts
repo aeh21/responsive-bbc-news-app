0ea0c83d9da249acf07dd27e61e8beac63ccb7f2
Arnold Haidu <arnoldhaidu@gmail.com>
2023-01-28 10:58:06 +0200

    fix: return the list of articles from api
    - updates return value to articles
    - extracts `Article` structure into interface from response

--- src/api/useFetchTopHeadlines/index.ts
+++ src/api/useFetchTopHeadlines/index.ts
@@ -3,11 +3,27 @@ import useSWR from "swr";
 import axiosInstance from "../../axios/instance";
 import { NEWS_API_URL } from "../../constants";
 
+interface Article {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
 const useFetchTopHeadlines = (countryCode: string | undefined) => {
   const cacheKey = `${NEWS_API_URL}-${countryCode}`;
 
-  return useSWR<any, unknown>(cacheKey, () =>
-    axiosInstance.get(`${NEWS_API_URL}/top-headlines?country=${countryCode}`).then((response) => response.data)
+  return useSWR<Article[], unknown>(cacheKey, () =>
+    axiosInstance
+      .get(`${NEWS_API_URL}/top-headlines?country=${countryCode}`)
+      .then((response) => response.data.articles)
   );
 };
 
